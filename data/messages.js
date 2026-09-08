5edf1d26fd79b2d4d00927c8a9ef0a14509cf05c
Emanuel Bahr <bahr.emanuel@gmail.com>
2026-09-08 09:09:35 +0200

    Update messages.js

--- data/messages.js
+++ data/messages.js
@@ -1,7 +1,7 @@
 const FRIENDS_MESSAGES = [
   {
     from: "Emu, Hanni, Katha, Lukki",
-    text: "Für eure gemeinsame Ehe-Quest wünschen wir euch von Herzen alles, was wirklich zählt: tiefes Vertrauen, ein Zuhause voller Geborgenheit und diese bedingungslose Liebe.\n\n Weil die Ehe aber mindestens so viel Spaß machen soll wie unsere gemeinsamen Abende, wünschen wir euch dazu das ultimative Würfelglück! Möget ihr im Alltag immer die besten Zahlen werfen – sei es beim Verhandeln, wer den Müll rausbringt, oder wenn das Leben mal eine spontane Hürde aufstellt. Damit euch der Nachschub dafür nie ausgeht, gilt für euer Zuhause die goldene Regel: Hört erst auf einzurichten, wenn mindestens ein Schrank randvoll mit Spielen steht! Wir freuen uns riesig auf alles, was kommt – Auf euch und euer perfektes Match!",
+    text: "Für eure gemeinsame Ehe-Quest wünschen wir euch von Herzen alles, was wirklich zählt: tiefes Vertrauen, ein Zuhause voller Geborgenheit und diese bedingungslose Liebe.\n\n Weil die Ehe aber mindestens so viel Spaß machen soll wie unsere gemeinsamen Abende, wünschen wir euch dazu das ultimative Würfelglück! Möget ihr im Alltag immer die besten Zahlen werfen – sei es beim Verhandeln, wer den Müll rausbringt, oder wenn das Leben mal eine spontane Hürde aufstellt.\n\n Damit euch der Nachschub dafür nie ausgeht, gilt für euer Zuhause die goldene Regel: Hört erst auf einzurichten, wenn mindestens ein Schrank randvoll mit Spielen steht!\n\n Wir freuen uns riesig auf alles, was kommt – Auf euch und euer perfektes Match!",
     color: "#ffd166",
   }
 ];
